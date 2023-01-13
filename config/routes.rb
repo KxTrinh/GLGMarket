c6493ad5fb78a1dd241d1f15994fe6f9cd9e2f1f
Kevin Trinh <kxt.trinh@gmail.com>
2023-01-13 16:08:42 +0100

    post model and product model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+
+  root to: 'pages#home'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :products, only: %i[index show new create] do
+    resources :posts, only: %i[new create update]
+  end
+  resources :posts, only: %i[destroy]
+
 end
